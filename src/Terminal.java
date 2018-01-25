397e7ab60167ba4c382b28537676ef6ff56d06e5
Joe <joedarling7@gmail.com>
2018-01-24 19:54:01 -0500

    Created terminal class to override TextArea

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,7 +1,24 @@
-package PACKAGE_NAME;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 
 /**
  * Created by Joe on 1/24/2018.
  */
-public class Terminal {
+public class Terminal extends TextArea {
+    @Override
+    public void replaceText(int start, int end, String text) {
+        String current = getText();
+        // only insert if no new lines after insert position:
+        if (! current.substring(start).contains("\n")) {
+            super.replaceText(start, end, text);
+        }
+    }
+    @Override
+    public void replaceSelection(String text) {
+        String current = getText();
+        int selectionStart = getSelection().getStart();
+        if (! current.substring(selectionStart).contains("\n")) {
+            super.replaceSelection(text);
+        }
+    }
 }
