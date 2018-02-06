0d0bda3440912bb300fe9f0a00784725d7c1ae3d
Joe <joedarling7@gmail.com>
2018-02-06 18:09:31 -0500

    Added edit mode and overrided appendText method

--- src/Terminal.java
+++ src/Terminal.java
@@ -5,11 +5,18 @@ import javafx.scene.input.KeyCode;
  * Created by Joe on 1/24/2018.
  */
 public class Terminal extends TextArea {
+
+    private boolean inEditMode;
+
+    public Terminal(){
+        inEditMode = false;
+    }
+
     @Override
     public void replaceText(int start, int end, String text) {
         String current = getText();
         // only insert if no new lines after insert position:
-        if (! current.substring(start).contains("\n")) {
+        if (! current.substring(start).contains("\n") && inEditMode) {
             super.replaceText(start, end, text);
         }
     }
@@ -17,8 +24,15 @@ public class Terminal extends TextArea {
     public void replaceSelection(String text) {
         String current = getText();
         int selectionStart = getSelection().getStart();
-        if (! current.substring(selectionStart).contains("\n")) {
+        if (! current.substring(selectionStart).contains("\n") && inEditMode) {
             super.replaceSelection(text);
         }
+    }
+
+    @Override
+    public void appendText(String text){
+        inEditMode = true;
+        super.appendText(text);
+        inEditMode = false;
     }
 }
