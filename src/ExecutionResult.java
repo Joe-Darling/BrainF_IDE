c2f72d97eec024055d36809a330946e4b6d2a92e
Joe <joedarling7@gmail.com>
2018-02-07 03:12:01 -0500

    Commented files

--- src/ExecutionResult.java
+++ src/ExecutionResult.java
@@ -3,10 +3,10 @@
  * This class is returned by an execution of one command. It contains updated data of the pointer and character data.
  */
 public class ExecutionResult {
-    private boolean codeBroken;
-    private int pointer;
-    private int charReader;
-    private String message;
+    private boolean codeBroken; // Whether or not the previous command broke the program
+    private int pointer; // The index of the pointer currently
+    private int charReader; // Where the interpreter is in the code
+    private String message; // The message that resulted from the command being executed
 
     public ExecutionResult(int pointer, int charReader, boolean codeBroken, String message){
         this.pointer = pointer;
--- src/Terminal.java
+++ src/Terminal.java
@@ -9,12 +9,27 @@ public class Terminal extends TextArea {
     enum Mode{EDIT, STATIC}
 
     private Mode currMode;
-    private int charsPrintedThisLine;
+    private int charsPrintedThisLine; // The number of characters on the bottom most line of the terminal
 
     public Terminal(){
         currMode = Mode.STATIC;
     }
 
+    public Mode getCurrMode(){
+        return currMode;
+    }
+
+    public void setCurrMode(Mode newMode){
+        currMode = newMode;
+    }
+
+    /**
+     * This method overrides the replaceText method and only allows users to type text if they are on the bottom
+     * most row or the terminal and the terminal is in edit more.
+     * @param start Start index of replacement
+     * @param end End index of replacement
+     * @param text Text to place
+     */
     @Override
     public void replaceText(int start, int end, String text) {
         String current = getText();
@@ -23,6 +38,12 @@ public class Terminal extends TextArea {
             super.replaceText(start, end, text);
         }
     }
+
+    /**
+     * This method overrides the replaceSelection method and only allows users to paste text if they are on the bottom
+     * most row or the terminal and the terminal is in edit more.
+     * @param text the text to paste
+     */
     @Override
     public void replaceSelection(String text) {
         String current = getText();
@@ -32,9 +53,14 @@ public class Terminal extends TextArea {
         }
     }
 
+    /**
+     * This method overrides the append text method and writes to the terminal. After writing it ensures that
+     * the terminal is back in static mode.
+     * @param text The text to add to the terminal
+     */
     @Override
     public void appendText(String text){
-        currMode = Mode.EDIT;
+        currMode = Mode.EDIT; // This line may be unnecessary
         for(char c : text.toCharArray()){
             super.appendText(String.valueOf(c));
             charsPrintedThisLine++;
@@ -45,13 +71,5 @@ public class Terminal extends TextArea {
         }
 
         currMode = Mode.STATIC;
-    }
-
-    public Mode getCurrMode(){
-        return currMode;
-    }
-
-    public void setCurrMode(Mode newMode){
-        currMode = newMode;
     }
 }
