7e3c47b654969f562d09fdb44298f7aa17a759af
Joe <joedarling7@gmail.com>
2018-02-06 23:15:15 -0500

    Added a mode enum and changed how text is allowed to be appeneded

--- src/Terminal.java
+++ src/Terminal.java
@@ -6,18 +6,20 @@ import javafx.scene.input.KeyCode;
  */
 public class Terminal extends TextArea {
 
-    private boolean inEditMode;
+    enum Mode{EDIT, STATIC}
+
+    private Mode currMode;
     private int charsPrintedThisLine;
 
     public Terminal(){
-        inEditMode = false;
+        currMode = Mode.STATIC;
     }
 
     @Override
     public void replaceText(int start, int end, String text) {
         String current = getText();
         // only insert if no new lines after insert position:
-        if (! current.substring(start).contains("\n") && inEditMode) {
+        if (! current.substring(start).contains("\n") && currMode == Mode.EDIT) {
             super.replaceText(start, end, text);
         }
     }
@@ -25,14 +27,14 @@ public class Terminal extends TextArea {
     public void replaceSelection(String text) {
         String current = getText();
         int selectionStart = getSelection().getStart();
-        if (! current.substring(selectionStart).contains("\n") && inEditMode) {
+        if (! current.substring(selectionStart).contains("\n") && currMode == Mode.EDIT) {
             super.replaceSelection(text);
         }
     }
 
     @Override
     public void appendText(String text){
-        inEditMode = true;
+        currMode = Mode.EDIT;
         for(char c : text.toCharArray()){
             super.appendText(String.valueOf(c));
             charsPrintedThisLine++;
@@ -42,6 +44,14 @@ public class Terminal extends TextArea {
             }
         }
 
-        inEditMode = false;
+        currMode = Mode.STATIC;
+    }
+
+    public Mode getCurrMode(){
+        return currMode;
+    }
+
+    public void setCurrMode(Mode newMode){
+        currMode = newMode;
     }
 }
